f5f891e2e05cbb21b9bf0b2d472e64c30cfdc348
Non <Kitanon_san@cmu.ac.th>
2021-11-26 16:05:22 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,30 @@
 using namespace std;
 
 int main(){
+    int number[1000] ={};
+    int j = 0;
+
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> number[j];
+    while (number[j] != 0){
+        j++;
+        cout << "Enter an integer: ";
+        cin >> number[j];
+
+    }
+    int odd = 0,Even = 0;
+    int k=0;
+    while(number[k] != 0){
+        if(number[k]%2 == 0){
+            Even++;
+        }
+        else {
+            odd++;
+        }
+        k++;
+    }
+    cout << "#Even numbers = " << Even << endl;
+
+    cout << "#Odd numbers = " << odd;
     return 0;
 }
